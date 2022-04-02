992ab1f85e50fcf401a99103e144bee9328e015c
yeicobF <A291423@alumnos.uaslp.mx>
2022-04-01 21:00:30 -0600

    Fix: Eslint + Prettier config
    Ya funciona el linter y el Prettier con la configuración especificada.
    
    PROBLEMA:
    
    - Cuando activo airbnb, deja de funcionar el linter.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
+  root: true,
   // Agregar prettier para no tener problemas de compatibilidad con prettier y
   // eslint.
   extends: [
     "next/core-web-vitals",
     // https://www.npmjs.com/package/eslint-config-airbnb
-    "airbnb",
-    "airbnb/hooks",
     "prettier",
     // https://www.npmjs.com/package/eslint-plugin-prettier
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    // "airbnb",
+    // "airbnb/hooks",
   ],
   // extends: ["next/core-web-vitals", "airbnb", "airbnb/hooks", "prettier"],
   plugins: ["prettier"],
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto"
+      }
+    ],
     /**  https://stackoverflow.com/a/40458301/13562806
      *
      * 2 es un error, 1 es un warning, 0 es off.
      * Semicolon (punto y coma) llevarlo siempre.
      */
     semi: [2, "always"],
-    "space-before-function-paren": ["error", "always"]
+    // "space-before-function-paren": ["error", "always"]
+    "no-unused-vars": [2, "all"],
+    "no-unused-import": [1, "all"],
   }
 };
