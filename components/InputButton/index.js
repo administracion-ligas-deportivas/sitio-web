9f88a30445073fbb264f564e97d578b55e97d45d
XimenaRebolloso <A282526@alumnos.uaslp.mx>
2022-05-03 21:34:10 -0500

    Agregar modulos de estilos a los componentes
    Se agregaron los modulos de estilo para cada uno
    de los componentes.
    Y se asginaron los ClassName a cada uno.
    Estoy intentando agregar el modal de del
    Icon, para que el usuario seleccione una imagen
    o la elimine.

--- components/InputButton/index.js
+++ components/InputButton/index.js
@@ -1,8 +1,14 @@
+import styles from "../../styles/Inputbutton.module.css";
+
 export default function InputButtonComponent({ name, placeholder, content }) {
   return (
     // Seria el placeholder y el contenido del boton ?
-    <div className="inputbutton">
-      <input name={name} type="text" placeholder={placeholder} />
+    <div className={styles.inputbutton}>
+      <input
+        name={name}
+        type="text"
+        placeholder={placeholder}
+      />
       <button type="button">{content}</button>
     </div>
   );
