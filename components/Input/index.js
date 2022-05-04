7d6ac96014a412952920b17288a3a1bbbe2a7e5c
yeicobF <jacobrdz34@gmail.com>
2022-05-04 13:37:01 -0500

    feat: Agregar Label + Input generales, hay fallo
    Agregué un Label e Input tomando como base los estilos ya existentes,
    pero estoy teniendo problemas, ya que al agregar este componente dentro
    de InputCode, me muestra la estructura y estilos de antiguo Input (ahora
    OldInput). Por ahora, no he visto cómo solucionarlo.
    
    - Hay que normalizar los tamaños de las fuentes. Me gustaría que el
      Label tuviera una fuente más pequeña, pero igual puede quedar como em
      para ser proporcional al tamaño de la fuente del padre.

---
+++ components/Input/index.js
@@ -0,0 +1,27 @@
+import styles from "./Input.module.css";
+
+export default function Input({
+  id = "",
+  name = "",
+  type = "text",
+  title = "",
+  placeholder = "",
+  readOnly = false,
+  className,
+}) {
+  const classNames = className
+    ? [...className, styles.input].join(" ")
+    : styles.input;
+
+  return (
+    <input
+      name={name}
+      id={id}
+      type={type}
+      title={title}
+      placeholder={placeholder}
+      readOnly={readOnly}
+      className={classNames}
+    />
+  );
+}
--- components/InputCode/index.js
+++ components/InputCode/index.js
@@ -1,8 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
+/*  
+Hay que agregar directamente los estilos. Si no, PostCSS los purga.
+
+https://stackoverflow.com/a/66575373/13562806 
+*/
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import styles from "./InputCode.module.css";
 import Button from "../Button";
+import Label from "../Label";
+import Input from "../Input";
 
 // U parametros del Usuario
 // Molecula
@@ -14,25 +22,45 @@ export default function InputCodeComponent({
   contentU,
 }) {
   return (
-    // Seria el placeholder y el contenido del boton ?
-    <div className={styles.inputbutton}>
-      <InputComponent
-        title={titleU}
-        name={nameU}
-        placeholder={placeholderU}
-      />
-      <Button>{contentU}</Button>
-      <Button
-        variant="primary"
-        inverse
-      >
-        <FontAwesomeIcon
-          icon={faCopy}
-          className={styles.copyIcon}
+    <>
+      <div className={styles.container}>
+        <Label
+          content={titleU}
+          htmlFor={nameU}
         />
-      </Button>
-      {/* <CopyComponent /> */}
-    </div>
+
+        <div className={styles.container}>
+          <Input
+            id={nameU}
+            name=""
+            type="text"
+            title=""
+            placeholder=""
+            readOnly
+          />
+        </div>
+      </div>
+      {/* Seria el placeholder y el contenido del boton ? */}
+      <div className={styles.inputbutton}>
+        <InputComponent
+          title={titleU}
+          name={nameU}
+          placeholder={placeholderU}
+        />
+        <Button>{contentU}</Button>
+        <Button
+          variant="primary"
+          inverse
+        >
+          <FontAwesomeIcon
+            icon={faCopy}
+            className={styles.copyIcon}
+            // size="2x"
+          />
+        </Button>
+        {/* <CopyComponent /> */}
+      </div>
+    </>
   );
 }
 
