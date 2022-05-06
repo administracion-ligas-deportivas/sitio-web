70a182329a02c9ccec9c3bde04500b749b89af8e
XimenaRebolloso <A282526@alumnos.uaslp.mx>
2022-05-06 18:06:25 -0500

    Pantallas Login y Signup completas.
    Ambas pantallas están completas, puede que en el futuro necesiten modificaciones pero hasta ahora funcionan de la manera esperada.

---
+++ components/Button.js
@@ -0,0 +1,119 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable @next/next/no-css-tags */
+
+function button() {
+  return (
+    <div>
+      <button
+        type="button"
+        href="www.google.com"
+        style={{
+          backgroundColor: "rgb(30, 20, 87)",
+          borderRadius: "10px",
+          color: "white",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(223, 29, 45)",
+          borderRadius: "10px",
+          color: "white",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(26, 28, 158)",
+          borderRadius: "10px",
+          color: "white",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(60, 100, 177)",
+          borderRadius: "10px",
+          color: "white",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(243, 88, 101)",
+          borderRadius: "10px",
+          color: "white",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(239, 239, 239)",
+          borderRadius: "10px",
+          color: "black",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(244, 246, 251)",
+          borderRadius: "10px",
+          color: "black",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "rgb(255, 245, 246)",
+          borderRadius: "10px",
+          color: "black",
+          fontWeight: "bold",
+          height: "40px",
+          width: "70px",
+        }}
+      >
+        Botón
+      </button>
+    </div>
+  );
+}
+export default button;
---
+++ pages/equipos/equipo-hola.js
@@ -0,0 +1,3 @@
+export default function Hola() {
+  return <div>Hola</div>;
+}
