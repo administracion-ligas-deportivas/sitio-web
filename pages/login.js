1a2a0c168a824f522570837be2a036dafa08ec10
XimenaRebolloso <A282526@alumnos.uaslp.mx>
2022-05-05 21:45:47 -0500

    Pantalla de Login creada.
    Aun hay cosas pendientes, los componentes no eran responsivos
    y se trato de arreglar.
    Queda pendiente el padding, que el contenido principal se adapte
    a todo el ancho.

---
+++ pages/login.js
@@ -0,0 +1,74 @@
+// import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import myimg from "../public/img-login.png";
+import Footer from "../components/Footer";
+import styles from "../styles/LoginSignup.module.css";
+import Input from "../components/Input/index";
+import Label from "../components/Label";
+import Button from "../components/Button/index";
+
+function Login() {
+  const imageClasses = [styles.container, "hidden", "sm:flex"].join(" ");
+
+  return (
+    <div className="flex flex-col justify-between h-full">
+      <section className={[styles.box]}>
+        <div className={imageClasses}>
+          <h1 className={styles.titleLeague}>
+            Administración de Ligas Deportivas
+          </h1>
+          <Image
+            src={myimg}
+            width="300px"
+            height="300px"
+          />
+        </div>
+        <div className={[styles.container, styles.loginContainer].join(" ")}>
+          <h1 className={styles.titlePage}>Inicia sesión</h1>
+          <form
+            action=""
+            className={styles.formContainer}
+          >
+            <Label
+              htmlFor="user-label"
+              content="Correo o teléfono"
+            >
+              <Input
+                id="user-input"
+                placeholder="Correo o teléfono"
+              />
+            </Label>
+            <Label
+              htmlFor="user-label-pass"
+              content="Contraseña"
+            >
+              <Input
+                id="user-input-pass"
+                placeholder="Contraseña"
+              />
+            </Label>
+            <Link
+              href="./signup"
+              passHref
+            >
+              <a
+                href="dummy-href"
+                className={styles.elementsContainerAnchor}
+              >
+                {" "}
+                Regístrate ahora{" "}
+              </a>
+            </Link>
+            <Button variant="primary">
+              <p>Iniciar sesión</p>
+            </Button>
+          </form>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default Login;
